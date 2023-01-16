7d9b1851c5d0f5454d97ca6c38ad8e2843f01199
Shivam Sinha <shivam.sinha9966@gmail.com>
2023-01-17 02:21:29 +0530

    GetById

--- src/main/java/com/springboot/crud_app/controller/CustomerController.java
+++ src/main/java/com/springboot/crud_app/controller/CustomerController.java
@@ -1,8 +1,10 @@
 package com.springboot.crud_app.controller;
 
+import com.springboot.crud_app.exception.ResourceNotFoundException;
 import com.springboot.crud_app.model.Customer;
 import com.springboot.crud_app.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,5 +22,12 @@ public class CustomerController {
     @PostMapping
     public Customer createCustomer(@RequestBody Customer customer) {
         return customerRepository.save(customer);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable long id) {
+        Customer customer = customerRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Customer with ID " + id + " Does not Exist."));
+        return ResponseEntity.ok(customer);
     }
 }
